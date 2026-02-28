65ae4787308ce6ff205c2c3edd9bd8dbe37524e8
mohammadr1 <fadak2332@gmail.com>
2026-02-28 12:49:18 +0330

    fixed error

--- resources/views/customer/layouts/footer.blade.php
+++ resources/views/customer/layouts/footer.blade.php
@@ -42,7 +42,7 @@
         <div class="col-md-2 mb-3">
         <h5 class="font-vazir-bold">مجوزها</h5>
         <div>
-            <a href="https://e-rasaneh.ir/Certificate/76599" target="_blank">
+            <a href="#" target="_blank">
             <img src="{{ asset('assets/img/mojavez.jpg') }}" width="100" />
             </a>
         </div>
